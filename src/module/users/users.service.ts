2ec54f74e017bfcd6708c286c27d24de10074de0
Hyosik Philip Joo <lux1937@gmail.com>
2024-05-07 22:02:11 +0900

    Feat: Inject user model in user service

--- src/module/users/users.service.ts
+++ src/module/users/users.service.ts
@@ -1,4 +1,9 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+}
