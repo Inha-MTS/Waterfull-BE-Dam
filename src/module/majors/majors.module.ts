7996a0967d68407a950249e2aa0beb4a5b1d9236
Hyosik Philip Joo <lux1937@gmail.com>
2024-05-14 21:00:17 +0900

    Feat: Import Major schema in major module

--- src/module/majors/majors.module.ts
+++ src/module/majors/majors.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MajorsService } from './majors.service';
 import { MajorsController } from './majors.controller';
+import { Major, MajorSchema } from './entities/major.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Major.name, schema: MajorSchema }]),
+  ],
   controllers: [MajorsController],
   providers: [MajorsService],
 })
