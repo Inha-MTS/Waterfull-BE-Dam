1aae82ffeebfdc0237e4c77c4606a3dd4ce2879e
Hyosik Philip Joo <lux1937@gmail.com>
2024-05-14 21:20:32 +0900

    Feat: Implement CreateMajor controller method

--- src/module/majors/majors.controller.ts
+++ src/module/majors/majors.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { MajorsService } from './majors.service';
+import { CreateMajorDto } from './dto/create-major.dto';
 
 @Controller('majors')
 export class MajorsController {
   constructor(private readonly majorsService: MajorsService) {}
+
+  /**
+   * @route POST /majors
+   * @desc Create Major
+   * @access Public
+   */
+
+  @Post()
+  async createMajor(@Body() createMajorDto: CreateMajorDto): Promise<void> {
+    await this.majorsService.createMajor(createMajorDto);
+  }
 }
