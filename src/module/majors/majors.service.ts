13574e6a6d461683705ab7ff21ac33f773841d84
Hyosik Philip Joo <lux1937@gmail.com>
2024-05-14 21:01:14 +0900

    Feat: Inject major model in major service

--- src/module/majors/majors.service.ts
+++ src/module/majors/majors.service.ts
@@ -1,4 +1,9 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Major } from './entities/major.entity';
 
 @Injectable()
-export class MajorsService {}
+export class MajorsService {
+  constructor(@InjectModel(Major.name) private MajorModel: Model<Major>) {}
+}
