079b608f525be0a9c47daaa73e6118686f66e10a
Hyosik Philip Joo <lux1937@gmail.com>
2024-05-14 21:20:20 +0900

    Feat: Implement CreateMajor service method

--- src/module/majors/majors.service.ts
+++ src/module/majors/majors.service.ts
@@ -2,8 +2,13 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Major } from './entities/major.entity';
+import { CreateMajorDto } from './dto/create-major.dto';
 
 @Injectable()
 export class MajorsService {
   constructor(@InjectModel(Major.name) private MajorModel: Model<Major>) {}
+  async createMajor(createMajorDto: CreateMajorDto) {
+    const createdMajor = new this.MajorModel(createMajorDto);
+    return createdMajor.save();
+  }
 }
