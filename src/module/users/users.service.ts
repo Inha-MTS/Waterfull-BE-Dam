514229a599139327de8da2cf03c1fe99893f0a29
Hyosik Philip Joo <lux1937@gmail.com>
2024-05-21 23:14:31 +0900

    Feat: Initial implementation of createUser with image handling

--- src/module/users/users.service.ts
+++ src/module/users/users.service.ts
@@ -2,8 +2,26 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const { images: imageStrings, type } = createUserDto;
+    const images = imageStrings.map((imageString: string) =>
+      Buffer.from(
+        imageString.replace(/^data:image\/\w+;base64,/, ''),
+        'base64',
+      ),
+    );
+    if (type === 'card') {
+      // use ocr to verify the user
+    }
+    if (type === 'face') {
+      // use face recognition to verify the user
+    }
+    return images;
+  }
 }
