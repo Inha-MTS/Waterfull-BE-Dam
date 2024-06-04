98e8fd91eb5420820a1256dc71b00bdab1fd56ff
Hyosik Philip Joo <lux1937@gmail.com>
2024-06-04 21:51:15 +0900

    Feat: Implement registerFaceId service

--- src/module/users/users.service.ts
+++ src/module/users/users.service.ts
@@ -4,6 +4,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { HttpService } from '@nestjs/axios';
+import { RegisterFaceImageDto } from './dto/register-face-image.dto';
+import { firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class UsersService {
@@ -19,5 +22,25 @@ export class UsersService {
       message: 'CREATED USER',
       data: { id },
     };
+  }
+
+  async registerFaceId(registerFaceImageDto: RegisterFaceImageDto) {
+    const { id, image } = registerFaceImageDto;
+    try {
+      const {
+        data: { face_id: faceId },
+      } = await firstValueFrom(
+        this.httpService.post(process.env.FACE_REGISTRATION_LAMBDA_URL, {
+          image,
+        }),
+      );
+
+      console.log(id, faceId); // insert face id here
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        throw new Error('Axios error happened');
+      }
+      throw new Error('DB error happened');
+    }
   }
 }
