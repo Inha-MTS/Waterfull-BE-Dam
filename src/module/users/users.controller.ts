5952b5658ecc47f45f7c85d98469efbdfbf02d2f
Hyosik Philip Joo <lux1937@gmail.com>
2024-06-04 20:16:40 +0900

    Feat: Implement createUser controller

--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  /**
+   * @route POST /users
+   * @desc Create User
+   * @access Public
+   */
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
 }
