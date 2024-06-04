622e5d68d9e3ad430a0915da03c0c2d30a9a8ed5
Hyosik Philip Joo <lux1937@gmail.com>
2024-06-04 21:19:20 +0900

    Add: Install and setup axios

--- src/module/users/users.service.ts
+++ src/module/users/users.service.ts
@@ -3,10 +3,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private readonly httpService: HttpService,
+  ) {}
 
   async createUser(createUserDto: CreateUserDto) {
     const newUser = new this.userModel(createUserDto);
