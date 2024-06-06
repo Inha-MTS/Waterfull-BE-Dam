00bff37a39f8e315627b9085a693c64b7020cae9
Hyosik Philip Joo <lux1937@gmail.com>
2024-06-06 21:11:48 +0900

    Feat: Implement loginUser controller

--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { RegisterFaceImageDto } from './dto/register-face-image.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -15,6 +22,25 @@ export class UsersController {
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
+  }
+
+  /**
+   * @route POST /users/login
+   * @desc Login User
+   * @access Public
+   */
+  @Post('/login')
+  async loginUser(@Body() loginUserDto: LoginUserDto) {
+    const { type, id, image } = loginUserDto;
+    if ((type === 'face' && !image) || (type === 'card' && !id)) {
+      throw new HttpException(
+        {
+          message: 'Missing required fields',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.usersService.loginUser(loginUserDto);
   }
 
   /**
